7dda650872846efea39df29adf4a3c726c88982d
Moises <mocaor2007@gmail.com>
2025-09-24 21:06:01 +0200

    Ejercicio 4

---
+++ src/Ejer4.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Ejer4 {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Introduce un numero positivo");
+        int num = sc.nextInt();
+        while (num <= 0);
+        int suma = 0;
+        for (int i = 1; i < num ; i++) {
+            suma += i;
+
+        }
+        System.out.println("La suma de los " +num+ " primeros numeros es: " + suma);
+
+
+    }
+}
