1c4060d150579022f95cda0d0dacec2456018b49
Moises <mocaor2007@gmail.com>
2025-09-25 13:37:54 +0200

    ola

--- src/Boletin1/Ejer6.java
+++ src/Boletin1/Ejer6.java
@@ -1,7 +1,21 @@
 package Boletin1;
 
+import java.util.Scanner;
+
 public class Ejer6 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce tu primer numero: ");
+        int numA = sc.nextInt();
+        System.out.println("Introduce tu segundo numero: ");
+        int numB = sc.nextInt();
+
+        int total = 0;
+        for (int i = 0; i < numB ; i++) {
+            total += numA;
+
+        }
+        System.out.println("El resultado es " + total);
 
     }
 }
