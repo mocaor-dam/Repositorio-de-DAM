a19891d267e7cf1504e44583655f3609ac70a6de
Moises <mocaor2007@gmail.com>
2025-09-25 13:59:15 +0200

    ola

---
+++ src/Boletin1/Ejer7.java
@@ -0,0 +1,21 @@
+package Boletin1;
+
+import java.util.Scanner;
+
+public class Ejer7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Cuantos numeros quieres introducir ");
+        int num = sc.nextInt();
+        System.out.println("Introduce " +num+ " numeros");
+        double suma = 0;
+
+        for (int i = 1; i <= num; i++) {
+            System.out.println("Introduce el " +i+ " numero:");
+            int num1 = sc.nextInt();
+            suma += num1;
+        }
+       double media = suma / num;
+        System.out.println("La media de los numeros es " +media);
+    }
+}
