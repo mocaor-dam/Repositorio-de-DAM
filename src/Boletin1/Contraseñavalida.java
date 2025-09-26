7ae676c7cbab26999807078b7acb407212901911
Moises <mocaor2007@gmail.com>
2025-09-26 12:05:46 +0200

    ola

--- src/Boletin1/Contraseñavalida.java
+++ src/Boletin1/Contraseñavalida.java
@@ -1,4 +1,5 @@
-import java.sql.SQLOutput;
+package Boletin1;
+
 import java.util.Scanner;
 
 public class Contraseñavalida {
---
+++ src/Boletin1/Ejer8.java
@@ -0,0 +1,38 @@
+package Boletin1;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Ejer8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        boolean continuar = true;
+        System.out.println("Introduzca un numero: ");
+        int num = sc.nextInt();
+        while (continuar) {
+            int menor = num;
+
+            System.out.println("¿Desea introducir más numeros?(S/N)");
+            String respuesta = sc.next().toUpperCase();
+
+            if (respuesta.equalsIgnoreCase("S")) {
+                System.out.println("Introduce un numero entonces");
+                num = sc.nextInt();
+                if (num < menor){
+                    menor = num;
+                }
+            } else if (respuesta.equalsIgnoreCase("N")) {
+                System.out.println("Aqui termina");
+                continuar =  false;
+            }
+            else{
+                System.out.println("El caracter debe ser S/N");
+            }
+
+            System.out.println("El numero menor de los introducidos es " + menor);
+        }
+
+        sc.close();
+    }
+}
