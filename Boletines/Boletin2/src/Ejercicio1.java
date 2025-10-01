8cf4c25aceeaddc7407d3443e4ce999ecfebfc30
Moises <mocaor2007@gmail.com>
2025-10-01 10:20:58 +0200

    olaa

--- Boletines/Boletin2/src/Ejercicio1.java
+++ Boletines/Boletin2/src/Ejercicio1.java
@@ -3,25 +3,26 @@ import java.util.Scanner;
 public class Ejercicio1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Introduce 3 numeros");
-        System.out.println("Introduce tu primer numero");
-        int num1 = sc.nextInt();
-        System.out.println("Introduce tu segundo numero");
-        int num2 = sc.nextInt();
-        System.out.println("Introduce tu tercer numero");
-        int num3 = sc.nextInt();
-
-        if (num1 > num2 && num2 > num3){
-            System.out.printf("El orden es "+num1+ " > " +num2+ " > " +num3);
-
+        System.out.println("Introduce 3 numeros: ");
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        int n3 = sc.nextInt();
+        if (n1 < n2 && n2 < n3) {
+            System.out.println(n1 + " < " + n2 + " < " + n3);
         }
-        else if (num1<num2 && num1 > num3){
-            System.out.println("El orden es " +num2+ " > " +num3+ " > "+ num1);
+        else if (n1 < n3 && n3 < n2) {
+            System.out.println(n1 + " < " + n3 + " < " + n2);
         }
-        else if (num1>num2 && num2<num3){
-            System.out.printf("El orden es "  +num1+ " > " +num3+ " > "+ num2);
-
+        else if (n2 < n1 && n1 < n3) {
+            System.out.println(n2 + " < " + n1 + " < " + n3);
         }
-
+        else if (n2 < n3 && n3 < n1) {
+            System.out.println(n2 + " < " + n3 + " < " + n1);
+        }
+        else if (n3 < n2 && n2 < n1) {
+            System.out.println(n3 + " < " + n2 + " < " + n1);
+        }
+        else
+            System.out.println(n3 + " < " + n1 + " < " + n2);
     }
 }
