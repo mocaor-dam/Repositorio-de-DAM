af664325275ff248da58238618098376d3d9449d
Moises <mocaor2007@gmail.com>
2025-10-17 09:31:26 +0200

    Arrays

---
+++ Boletines/Boletin Arrays/src/Ejercicio7.java
@@ -0,0 +1,21 @@
+public class Ejercicio7 {
+    static void main(String[] args) {
+        int[] miarray = {1, 2, 3, 4, 5, 6};
+        if (contieneElemento(miarray, 5)) {
+            System.out.println("El elemento esta en el array");
+        } else {
+            System.out.println("El elemento no esta en el array");
+        }
+    }
+
+    public static boolean contieneElemento(int[] array, int elementoABuscar) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == elementoABuscar) {
+                return true;
+            }
+        }
+        return false;
+
+    }
+}
+}
