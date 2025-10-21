fe564627a6567c7464ff6a3a2d86703a28aceb0e
Moises <mocaor2007@gmail.com>
2025-10-21 13:09:25 +0200

    Arrays

--- Boletines/Boletin Arrays/src/Ejercicio10.java
+++ Boletines/Boletin Arrays/src/Ejercicio10.java
@@ -1,5 +1,29 @@
-public class Ejercicio10 {
-    static void main(String[] args) {
+import java.util.Arrays;
 
+public class Ejercicio10 {
+    public static void main() {
+        int[] numeros = new int[10];
+        int size = 0;
+        for (int i = 0; i < numeros.length; i++) {
+            numeros[i] = MiEntradaSalida.generaAleatorio(numeros.length);
+        }
+        int[] numeros2 = new int[10];
+        for (int i = 0; i < numeros2.length; i++) {
+            numeros2[i] = MiEntradaSalida.generaAleatorio(numeros2.length);
+        }
+        System.out.println(Arrays.toString(unirArraysComunes(numeros, numeros2)));
+    }
+
+    public static int[] unirArraysComunes(int[] lista1, int[] lista2) {
+        int size = 10;
+        Arrays.sort(lista1);
+        Arrays.sort(lista2);
+        int[] comunes = new int[size];
+        for (int i = 0; i < 10; i++) {
+            if (lista1[i] == lista2[i]) {
+                comunes[i] = lista1[i];
+            }
+        }
+        return comunes;
     }
 }
---
+++ Boletines/Boletin Arrays/src/Ejercicio11.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class Ejercicio11 {
+    static void main(String[] args) {
+        int[] array1 = {10, 42, 74, 32, 61, 93, 22};
+        int[] array2 = {42, 31, 99, 45, 22, 82, 55, 77};
+        System.out.println(Arrays.toString(elementosEnComun(array1, array2)));
+    }
+
+    public static boolean encuentraElelemento(int[] array, int valorEncontrado) {
+        for (int elemento : array) {
+            if (elemento == valorEncontrado) { // le damos una condicion del elemento dentro  del array si esta o no esta
+                return true; //  en todo caso de que esté  devuelve verdadero
+            }
+        }
+        return false; // en caso de que no está  devuelve falso
+    }
+
+    public static int[] elementosEnComun(int[] array1, int[] array2) {
+        int contador = 0;
+        int[] arrayNuevo = new int[array1.length];
+        int comparar = Math.min(array1.length, array2.length);
+        for (int i = 0; i < comparar; i++) {
+            if (encuentraElelemento(array1, array2[i]) && !encuentraElelemento(arrayNuevo, array2[i])) {
+                arrayNuevo[contador++]=array2[i];
+            }
+        }
+        arrayNuevo = Arrays.copyOf(arrayNuevo,contador);
+        return arrayNuevo;
+    }
+}
