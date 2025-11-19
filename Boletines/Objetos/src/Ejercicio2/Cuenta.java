f37fda2297f0b0afecbb2e400c7de416bdb6e3ab
Moises <mocaor2007@gmail.com>
2025-11-19 10:20:07 +0100

    excepciones

--- Boletines/Objetos/src/Ejercicio2/Cuenta.java
+++ Boletines/Objetos/src/Ejercicio2/Cuenta.java
@@ -1,21 +1,68 @@
 package Ejercicio2;
 
+import Exceptions.CuentaException;
+
 public class Cuenta {
     private double saldo;
     private int numRetiradas;
     private int numIngresos;
 
-    public Cuenta(){
-        saldo = 0;
-        numIngresos = 0;
-        numRetiradas = 0;
+    public Cuenta() throws CuentaException {
+        setSaldo(0);
+        setNumIngresos(0);
+        setNumRetiradas(0);
     }
 
-    public Cuenta(double saldoInicial){
-        saldo = saldoInicial;
-        numRetiradas = 0;
-        numIngresos = 0;
+    public Cuenta(double saldoInicial) throws CuentaException {
+        setSaldo(saldoInicial);
+        setNumRetiradas(0);
+        setNumIngresos(0);
     }
 
-    public retirarSaldo()
+    private void setSaldo(double saldo) throws CuentaException {
+        if (saldo < 0) {
+            throw new CuentaException("Tu saldo debe ser positivo");
+        }
+        this.saldo = saldo;
+    }
+
+    public int getNumRetiradas() {
+        return numRetiradas;
+    }
+
+    public void setNumRetiradas(int numRetiradas) {
+        this.numRetiradas = numRetiradas;
+    }
+
+    public int getNumIngresos() {
+        return numIngresos;
+    }
+
+    public void setNumIngresos(int numIngresos) {
+        this.numIngresos = numIngresos;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void reintegro(double dineroRetirado) throws CuentaException {
+        if (dineroRetirado < 0) {
+            throw new CuentaException("No puedes introducir numeros negativos");
+        }
+        if (dineroRetirado > saldo) {
+            throw new CuentaException("Saldo insuficiente. La cantidad a retirar es mayor que el saldo actual.");
+        }
+        this.saldo -= dineroRetirado;
+        this.numRetiradas++;
+
+    }
+
+    public void ingreso(double dineroIngreso) throws CuentaException {
+        if (dineroIngreso < 0) {
+            throw new CuentaException("No puedes ingresar dinero negativo");
+        }
+        this.saldo += dineroIngreso;
+        this.numIngresos++;
+    }
 }
--- Boletines/Objetos/src/Ejercicio2/Principal.java
+++ Boletines/Objetos/src/Ejercicio2/Principal.java
@@ -1,11 +1,61 @@
 package Ejercicio2;
 
+import Ejercicio2.Cuenta;
+import Exceptions.CuentaException;
+import utils.MiEntradaSalida2;
+
+import java.util.Scanner;
+
 public class Principal {
-    static void main(String[] args) {
-        System.out.println("Bienvenido a tu banco");
-        Cuenta cuenta = new Cuenta();
-        boolean operaciones = true;
+    public static void main(String[] args) {
+        Cuenta c = null;
+        while (c == null) {
+            try {
+                double saldoInicial = MiEntradaSalida2.solicitarEnteroPositivo("Introduce el saldo inicial");
+                c = new Cuenta(saldoInicial);
+            } catch (CuentaException e) {
+                System.out.println(e.getMessage());
+            }
+        }
 
+        int opcion;
 
+        do {
+            mostarMenu();
+
+            opcion = MiEntradaSalida2.solicitarEnteroEnRango("Escribe una opción: ", 0, 2);
+
+            switch (opcion) {
+                case 1:
+                    int dineroARetirar = MiEntradaSalida2.solicitarEnteroPositivo("¿Cuánto dinero desea retirar?");
+                    try {
+                        c.reintegro(dineroARetirar);
+                        System.out.printf("Su saldo actual es de %.2f%n", c.getSaldo());
+                    } catch (CuentaException e) {
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case 0:
+                    System.out.println("Gracias por utilizar este cajero. Adiós.");
+
+                break;
+                case 2:
+                    int dineroAIngresar = MiEntradaSalida2.solicitarEnteroPositivo("¿Cuánto dinero desea ingresar?");
+                    try {
+                        c.ingreso(dineroAIngresar);
+                        System.out.printf("Su saldo actual es de %.2f%n", c.getSaldo());
+                    }catch (CuentaException e){
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+            }
+        } while (opcion != 0);
+    }
+
+    public static void mostarMenu() {
+        System.out.println("¿Qué desea hacer?");
+        System.out.println("0. Salir");
+        System.out.println("1. Retirar");
+        System.out.println("2. Ingresar");
     }
 }
