d26d2b2f1cd3f0d4f814c10ce18b286c32743866
Moises <mocaor2007@gmail.com>
2025-12-16 14:01:12 +0100

    Herencias

--- Boletines/Herencias/src/Boletin5/Ejercicio1/Cuenta.java
+++ Boletines/Herencias/src/Boletin5/Ejercicio1/Cuenta.java
@@ -1,7 +1,23 @@
 package Boletin5.Ejercicio1;
 
-public abstract class Cuenta {
-    public abstract double credito();
-    public abstract double saldo();
+public  class Cuenta {
+    public double credito;
+    public double saldo;
 
+    public Cuenta(double credito, double saldo) {
+        this.credito = credito;
+        this.saldo = saldo;
+    }
+
+    public double getCredito() {
+        if (credito == null){
+
+            credito = 100;
+        }
+        return credito;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
 }
--- Boletines/Herencias/src/Boletin5/Ejercicio1/CuentaCredito.java
+++ Boletines/Herencias/src/Boletin5/Ejercicio1/CuentaCredito.java
@@ -1,6 +1,9 @@
 package Boletin5.Ejercicio1;
 
 public class CuentaCredito extends Cuenta{
+
+
+
     @Override
     public double credito() {
         return 0;
@@ -11,5 +14,5 @@ public class CuentaCredito extends Cuenta{
         return 0;
     }
 
-    public
+
 }
